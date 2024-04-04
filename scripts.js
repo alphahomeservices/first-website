3d293bae14385bf0db81e1283dd16acea9a1806b
mershter <100219272+mershter@users.noreply.github.com>
2024-04-04 18:07:26 -0400

    reworking

--- scripts.js
+++ scripts.js
@@ -2,3 +2,18 @@ $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active')
 });
+
+$('.testimonial-slider-container').slick({
+    autoplay: true, // Do we want it to autoplay? true or false
+    autoplaySpeed: 8000, // How long between each slide when auto-playing
+    speed: 500, // How fast is the transition in milliseconds
+    arrows: true, // Do you want to show arrows to trigger each slide
+    accessibility: true, // Enables keyboard tabbing and arrow key navigation
+    dots: true, // Enables the dots below to show how many slides
+    fade: false, // Changes the animate from slide to fade if true
+    infinite: true, // When true, means that it will scroll in a circle
+    pauseOnHover: true, // When true means the autoplay pauses when hovering
+    pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+   
+    
+});
